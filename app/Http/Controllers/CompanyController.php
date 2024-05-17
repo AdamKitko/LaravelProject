9f6ace75a7333be15f6ca10244e582939171b70b
Adam- <adamkitko@gmail.com>
2024-05-17 18:20:06 +0200

    -companies table created, adding and deleting companies from list, register, login, parts accessable only for logged in users

---
+++ app/Http/Controllers/CompanyController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Company;
+
+class CompanyController extends Controller
+{
+    use \Illuminate\Foundation\Validation\ValidatesRequests;
+
+    public function create(Request $request)
+    {
+        $this->validate($request, [
+            'name' => ['required', 'string', 'max:255']
+        ]);
+        Company::create($request->all());
+
+        return to_route('companies');
+    }
+
+    public function delete($id)
+    {
+        $company = Company::find($id);
+        $company->delete();
+
+        return to_route('companies');
+    }
+}
